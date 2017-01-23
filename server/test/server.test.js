1afd101ddfed834d57608543c43eafe2c4e96167
bkdev98 <bkdev98@gmail.com>
2017-01-23 23:10:28 +0700

    Add test case for GET /articles/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Article} = require('./../models/article');
 
 const articles = [{
+  _id: new ObjectID(),
   title: 'Occaecat consectetur minim labore ullamco quis reprehenderit excepteur officia.',
   body: 'Sit aute velit exercitation magna voluptate incididunt aliqua aute nostrud id ad ut ex sint labore labore duis. Est labore fugiat magna labore veniam fugiat anim tempor ex eiusmod aliquip amet adipisicing esse enim dolor aute. Sunt dolor pariatur reprehenderit enim dolore aliqua id nostrud id velit sit consectetur.'
 }, {
+  _id: new ObjectID(),
   title: 'Enim eiusmod veniam amet dolore cupidatat id deserunt amet id in nostrud cupidatat cillum sunt deserunt.',
   body: 'Ut cupidatat dolore ea mollit reprehenderit commodo incididunt nostrud pariatur. Proident qui mollit ad pariatur et dolor veniam tempor est magna do ipsum. Esse est ad do consectetur do exercitation adipisicing excepteur laboris non voluptate commodo magna anim.'
 }];
@@ -74,3 +77,31 @@ describe('GET /articles', () => {
       .end(done);
   });
 });
+
+describe('GET /articles/:id', () => {
+  it('should return article doc', (done) => {
+    request(app)
+      .get(`/articles/${articles[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.article.title).toBe(articles[0].title);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if article not found', (done) => {
+    var someID = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/articles/${someID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if non-object ids', (done) => {
+    request(app)
+      .get('/articles/123')
+      .expect(404)
+      .end(done);
+  });
+})
